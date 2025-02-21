170c8d41f5945c3981698bd277f864fca95f71e9
javierbrk <javierbrk@gmail.com>
2025-02-21 09:05:24 -0300

    setup env script

---
+++ src/embedded/tools/setup_environment.sh
@@ -0,0 +1,22 @@
+# Install dependencies
+sudo apt update && sudo apt install -y python3 pip git unzip curl
+
+# Install Arduino CLI
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# Configure Arduino CLI
+arduino-cli config init
+arduino-cli config set board_manager.additional_urls https://espressif.github.io/arduino-esp32/package_esp32_index.json
+arduino-cli core update-index
+arduino-cli core install esp32:esp32
+
+# Install required libraries
+arduino-cli lib install "WiFiManager"
+arduino-cli lib install "HTTPClient"
+arduino-cli lib install "Adafruit SCD30"
+
+# Compile and upload firmware
+arduino-cli compile --fqbn esp32:esp32:esp32 SendToGrafana.ino --build-path ./bins
+arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32 --input-dir ./bins
